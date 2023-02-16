778fe853fbd0aa7951e85b6df69c680101c120e3
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 18:11:24 +0100

    EntityRefById

--- dotNet/Contract/v1/Models/EntityRef.cs
+++ dotNet/Contract/v1/Models/EntityRef.cs
@@ -4,4 +4,8 @@ namespace System.Data.UDAS.v1.Models {
     public object[] KeyValues { get; set; }
     public string Label { get; set; } = string.Empty;
   }
+  public class EntityRefById {
+    public string Id { get; set; }
+    public string Label { get; set; } = string.Empty;
+  }
 }
