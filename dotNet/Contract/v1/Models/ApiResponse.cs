4826097b63aac52c002394f1c7058be9c906580d
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 17:00:41 +0100

    ApiResponse Fix

--- dotNet/Contract/v1/Models/ApiResponse.cs
+++ dotNet/Contract/v1/Models/ApiResponse.cs
@@ -1,13 +1,20 @@
 ﻿
 namespace System.Data.UDAS.v1.Models {
-  public class ApiResponse<T> {
-    public T Return { get; set; }
+  public class ApiResponse {
     public string Fault { get; set; }
-    public static ApiResponse<T> Ok(T returnValue) {
+
+    public static ApiResponse<T> Ok<T>(T returnValue) {
       return new ApiResponse<T>() { Return = returnValue };
     }
-    public static ApiResponse<T> Error(string faultMessage) {
+    public static ApiResponse<T> Error<T>(string faultMessage) {
       return new ApiResponse<T>() { Fault = faultMessage };
     }
   }
+
+  public class ApiResponse<T> {
+    public T Return { get; set; }
+    public string Fault { get; set; }
+   
+  }
+
 }
