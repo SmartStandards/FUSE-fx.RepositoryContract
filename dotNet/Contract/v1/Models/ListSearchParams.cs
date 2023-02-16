cbbee29908a71f0c50933e0effb5907749ca3734
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 18:22:41 +0100

    ListSearchParamsByParent

--- dotNet/Contract/v1/Models/ListSearchParams.cs
+++ dotNet/Contract/v1/Models/ListSearchParams.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace System.Data.UDAS.v1.Models {
   public class ListSearchParams<TSearchFilter> {
     public string SortingField { get; set; }
     public bool SordDescending { get; set; }
     public TSearchFilter SearchFilter { get; set; }
+    public int Pagesize { get; set; }
+    public int Pagenumber { get; set; }
+  }
+  public class ListSearchParamsByParent {
+    public string SortingField { get; set; }
+    public bool SordDescending { get; set; }
+    public string ParentId { get; set; }
     public int Pagesize { get; set; }
     public int Pagenumber { get; set; }
   }
