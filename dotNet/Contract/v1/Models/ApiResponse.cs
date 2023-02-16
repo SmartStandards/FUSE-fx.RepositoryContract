7f1b91a1a2543cb4f73291ae45adf7dcb776cc06
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 16:42:08 +0100

    EntityRef + ApiResponse

---
+++ dotNet/Contract/v1/Models/ApiResponse.cs
@@ -0,0 +1,7 @@
+﻿
+namespace System.Data.UDAS.v1.Models {
+  public class ApiResponse<T> {
+    public T Return { get; set; }
+    public string Fault { get; set; }
+  }
+}
---
+++ dotNet/Contract/v1/Models/EntityRef.cs
@@ -0,0 +1,7 @@
+﻿
+namespace System.Data.UDAS.v1.Models {
+  public class EntityRef {
+    public object[] KeyValues { get; set; }
+    public string Label { get; set; } = string.Empty;
+  }
+}
