dba31a14faec5d6d038c9615bbb38475284c0a30
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 17:24:51 +0100

    IEntityRepository public

--- dotNet/Contract/v1/[API]/IEntityRepository.cs
+++ dotNet/Contract/v1/[API]/IEntityRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data.UDAS.v1.Models;
 
 namespace System.Data.UDAS.v1 {
-  internal interface IEntityRepository<TEntity, TDbEntity, TSearchFilter> {
+  public interface IEntityRepository<TEntity, TDbEntity, TSearchFilter> {
     TEntity AddOrUpdate(TEntity entity);
     void DeleteEntities(IList<object[]> keyValuesCollection);
     PaginatedResponse<TEntity> Search(ListSearchParams<TSearchFilter> searchParams);
