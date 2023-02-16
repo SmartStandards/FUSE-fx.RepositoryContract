64022970365baf151747e828b9d484b902dc6242
Rafael Weschle <rafael.weschle@gmail.com>
2023-02-16 16:56:15 +0100

    ApiResponse Factories

--- dotNet/Contract/v1/Models/ApiResponse.cs
+++ dotNet/Contract/v1/Models/ApiResponse.cs
@@ -3,5 +3,11 @@ namespace System.Data.UDAS.v1.Models {
   public class ApiResponse<T> {
     public T Return { get; set; }
     public string Fault { get; set; }
+    public static ApiResponse<T> Ok(T returnValue) {
+      return new ApiResponse<T>() { Return = returnValue };
+    }
+    public static ApiResponse<T> Error(string faultMessage) {
+      return new ApiResponse<T>() { Fault = faultMessage };
+    }
   }
 }
