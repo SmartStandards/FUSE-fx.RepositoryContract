b0417522878bf1168b17b9409e7919305046be03
CI Build <ci@ushell.org>
2025-11-03 09:19:31 +0000

    Fix -> VERSIONING [skip ci]

--- doc/changelog.md
+++ doc/changelog.md
@@ -12,6 +12,12 @@ This files contains a version history including all changes relevant for semanti
 
 
 
+## v 0.5.13
+released **2025-11-03**, including:
+ - new revision without significant changes
+
+
+
 ## v 0.5.12
 released **2025-06-03**, including:
  - new revision without significant changes
--- dotnet/src/RepositoryContract.net48/Properties/AssemblyInfo.cs
+++ dotnet/src/RepositoryContract.net48/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("46adc8b8-2f14-4a31-87ca-95b71ee09ea9")]
 
-[assembly: AssemblyVersion("0.5.12")]
-[assembly: AssemblyInformationalVersion("0.5.12")]
-//DONT ADD: [assembly: AssemblyFileVersion("0.5.12")] !!!
+[assembly: AssemblyVersion("0.5.13")]
+[assembly: AssemblyInformationalVersion("0.5.13")]
+//DONT ADD: [assembly: AssemblyFileVersion("0.5.13")] !!!
