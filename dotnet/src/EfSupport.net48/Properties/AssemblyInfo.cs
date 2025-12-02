a2ec8975bd70624aea52ac718482e4d0b19ae142
CI Build <ci@ushell.org>
2025-12-02 21:47:11 +0000

    Unittests + Fixes in InMemoryRepository -> VERSIONING [skip ci]

--- dotnet/src/EfSupport.net48/Properties/AssemblyInfo.cs
+++ dotnet/src/EfSupport.net48/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("46adc8b8-2f14-4a31-87ca-95b71ee09ea8")]
 
-[assembly: AssemblyVersion("0.5.14")]
-[assembly: AssemblyInformationalVersion("0.5.14")]
-//DONT ADD: [assembly: AssemblyFileVersion("0.5.14")] !!!
+[assembly: AssemblyVersion("0.5.15")]
+[assembly: AssemblyInformationalVersion("0.5.15")]
+//DONT ADD: [assembly: AssemblyFileVersion("0.5.15")] !!!
